4d874c0f3386712907dadde36c13c5407674e129
Aasim <aasimshaibani25@email.com>
2025-04-14 14:52:47 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,3 @@
-/*import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-})*/
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -30,4 +9,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  base: '/Ideal-Cafe/', // Add your repository name here
 });
